49452fa613c84450e8a023a9758d34fb456d2f7b
ShubhamDev021 <shubhat021@gmail.com>
2025-09-23 17:45:19 +0530

    Objects and using interface in it to avoid duplication of code and maintaining code redundancy

---
+++ objects.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var user1 = {
+    id: 1,
+    name: 'Shubham',
+    isAdmin: true,
+};
+var user2 = {
+    id: 2,
+    name: 'Ayush'
+};
+console.log('user 1: ', user1);
+console.log('user 2: ', user2);
---
+++ objects.ts
@@ -0,0 +1,15 @@
+import { User } from "./interfaces/user";
+
+let user1: User = {
+    id: 1,
+    name: 'Shubham',
+    isAdmin: true,
+}
+
+let user2: User = {
+    id: 2,
+    name: 'Ayush'
+}
+
+console.log('user 1: ', user1);
+console.log('user 2: ', user2);
