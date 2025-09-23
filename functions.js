3ddd720ee5080e6e6add213d09584fddb061d6a2
ShubhamDev021 <shubhat021@gmail.com>
2025-09-23 17:30:32 +0530

    Function with required parameters

---
+++ functions.js
@@ -0,0 +1,6 @@
+// Function with required parameters
+function add(a, b) {
+    return a + b;
+}
+var sum = add(2, 3);
+console.log('Sum:', sum);
---
+++ functions.ts
@@ -0,0 +1,6 @@
+// Function with required parameters
+function add(a: number, b: number) : number {
+    return a + b;
+}
+let sum: number = add(2, 3);
+console.log('Sum:', sum);
