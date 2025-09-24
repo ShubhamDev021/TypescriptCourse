7bc8d8655680e3cefebd3b7781846c9f048cbc71
ShubhamDev021 <shubhat021@gmail.com>
2025-09-24 12:33:41 +0530

    Numeric enum with default value

---
+++ enums.js
@@ -0,0 +1,10 @@
+// Numeric enum
+var Direction;
+(function (Direction) {
+    Direction[Direction["Up"] = 0] = "Up";
+    Direction[Direction["Down"] = 1] = "Down";
+    Direction[Direction["Left"] = 2] = "Left";
+    Direction[Direction["Right"] = 3] = "Right";
+})(Direction || (Direction = {}));
+console.log("Up: ".concat(Direction.Up));
+console.log("Left: ".concat(Direction.Left));
---
+++ enums.ts
@@ -0,0 +1,9 @@
+// Numeric enum with default value
+enum Direction {
+    Up,     // 0
+    Down,   // 1
+    Left,   // 2
+    Right,  // 3
+}
+console.log(`Up: ${Direction.Up}`)
+console.log(`Left: ${Direction.Left}`)
