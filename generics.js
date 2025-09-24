12db949457444b7a1a7625e5b2212e57e9489277
ShubhamDev021 <shubhat021@gmail.com>
2025-09-24 12:02:26 +0530

    Generic with array

--- generics.js
+++ generics.js
@@ -4,3 +4,9 @@ function identity(value) {
 }
 console.log(identity('Shubham'));
 console.log(identity(5));
+// Generic with array
+function getFirstElement(arr) {
+    return arr[0];
+}
+console.log(getFirstElement(['apple', 'banana', 'grapes']));
+console.log(getFirstElement([5, 2, 3, 1]));
--- generics.ts
+++ generics.ts
@@ -5,3 +5,11 @@ function identity<T>(value: T) : T {
 
 console.log(identity<string>('Shubham'));
 console.log(identity<number>(5));
+
+// Generic with array
+function getFirstElement<T>(arr: T[]) : T {
+    return arr[0];
+}
+
+console.log(getFirstElement<string>(['apple', 'banana', 'grapes']));
+console.log(getFirstElement<number>([5, 2, 3, 1]));
